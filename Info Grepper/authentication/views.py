34ce44831bab4c3f306655e99aacc189846a10ec
krishpatel2383 <krishpatel2383@gmail.com>
2023-01-22 23:49:30 +0530

    adding project files

---
+++ Info Grepper/authentication/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,"authentication/index.html")
+
+def signup(request):
+    return render(request,"authentication/signup.html")
+
+def signin(request):
+    return render(request,"authentication/signin.html")
+
+def signout(request):
+    pass
