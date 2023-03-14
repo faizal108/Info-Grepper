2c7ce89ad0ee2cf37c45373823359f0a81abcb19
faizal108 <69467378+faizal108@users.noreply.github.com>
2023-03-14 22:25:09 +0530

    updates and error fixes

--- django_login_system-master/users/forms.py
+++ django_login_system-master/users/forms.py
@@ -3,11 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    first_name = forms.CharField(max_length=50, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=50, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=120, help_text='Required. Enter a valid email address.')
-    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+    first_name = forms.CharField(max_length=50, required=False)
+    last_name = forms.CharField(max_length=50, required=False)
+    email = forms.EmailField(max_length=120)
+    birth_date = forms.DateField()
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'birth_date', 'password1', 'password2')
+    
+    # def __init__(self, *args, **kwargs):
+    #     super(SignupForm,self).__init__(*args, **kwargs)
+
+    #     self.fields['username'].widget.attrs['class'] = 'form-control'
